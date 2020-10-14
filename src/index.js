86b8afaa8386e10dd2d130c6da1d4b73eaf97b1a
gksky <a.gutkovsky@gmail.com>
2020-10-14 15:33:46 +0100

    feat: implement toreadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+  str = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 
+  10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 
+  20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'};
+  var res = '';
+  if (!number) return 'zero';
+  if (number >= 100) {
+    res = str[Math.trunc(number / 100)] + ' hundred';
+    number = number - Math.trunc(number / 100) * 100;
+  }
+  if (number > 0 && number < 20) {
+    if (res.length > 0) res = res + ' ' + str[number]; else res = str[number];
+    return res;
+  } else if (number >= 20) {
+    if (res.length > 0) res = res + ' ' + str[Math.trunc(number / 10) * 10]; else res = str[Math.trunc(number / 10) * 10];
+    number = number - Math.trunc(number / 10) * 10;
+  }
+  if (number) res = res + ' ' + str[number];
+  return res;
 }
